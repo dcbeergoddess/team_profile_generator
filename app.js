4cd3c8761694c221ae7547edfa5fcad02175511b
Rachel Murray <dcbeergoddess@gmail.com>
2020-04-30 21:42:51 -0400

    cleaning up some extra code and adding specific questions for each role. Also added some code after homework session earlier tonight

--- app.js
+++ app.js
@@ -92,7 +92,7 @@ function createManager(){
     {
       type: "input",
       name: "managerName",
-      message: "What is your Manager's name?",
+      message: "What is your name?",
       // Note how the validate function works
       validate: answer => {
         if (answer !== "") {
@@ -104,20 +104,25 @@ function createManager(){
     {
       type: "input",
       name: "managerId",
-      message: "Enter Manager ID"
+      message: "Enter Your ID"
     },
     {
       type: "input",
       name: "managerEmail",
-      message: "Enter Manager Email"
+      message: "Enter Your Email"
+    },
+    {
+      type: "input",
+      name: "officeNumber",
+      message: "Enter Your Office Number"
     }
     
-    // STUDENT: Add other questions here!
+   
 
     ]).then(response  => {
       // STUDENT: Process the response by instatiating a new object in the Manager class
       // process all the answers
-      const managerObj = new Manager (response.managerName, response.managerId, response.managerEmail)
+      const managerObj = new Manager (response.managerName, response.managerId, response.managerEmail, response.officeNumber)
       teamMembers.push(managerObj)
       start();
 
@@ -149,16 +154,20 @@ function createEngineer(){
     },
     {
       type: "input",
-      name: "EngineerEmail",
+      name: "engineerEmail",
       message: "Enter Engineer Email"
+    },
+    {
+      type: "input",
+      name: "gitHub",
+      message: "Enter Engineer Git Hub Username"
     }
     
-    // STUDENT: Add other questions here!
+  
 
     ]).then(response  => {
-      // STUDENT: Process the response by instatiating a new object in the Manager class
-      // process all the answers
-      const engineerObj = new Manager (response.engineerName, response.engineerId, response.engineerEmail)
+     
+      const engineerObj = new Manager (response.engineerName, response.engineerId, response.engineerEmail, response.gitHub)
       teamMembers.push(engineerObj)
       start();
 
@@ -191,6 +200,11 @@ function createIntern(){
       type: "input",
       name: "internEmail",
       message: "Enter Intern Email"
+    },
+    {
+      type: "input",
+      name: "school",
+      message: "Enter Intern's School" 
     }
     
     // STUDENT: Add other questions here!
@@ -198,75 +212,15 @@ function createIntern(){
     ]).then(response  => {
       // STUDENT: Process the response by instatiating a new object in the Manager class
       // process all the answers
-      const internObj = new Manager (response.internName, response.internId, response.internEmail )
+      const internObj = new Manager (response.internName, response.internId, response.internEmail, response.school )
       teamMembers.push(internObj)
       start();
 
-
-      // Now call the next question set
-      // createTeam(); (commented out from gary example)
     });
 }
 
-// This function starts team creation.
-function createTeam() {
-  inquirer.prompt([
-    // STUDENT: Ask which type of team member should be created with a list of choices
+start();
 
-  ]).then(userChoice => {
-    // STUDENT: Based on which choice they make, call the correct function to ask more questions.
-    // If no choice is made, then go to the rendering function.
-
-
-  });
+function finish(){
+  const html = render()
 }
-
-// This function starts team creation.
-function createEngineer() {
-  inquirer.prompt([
-    // STUDENT:  Engineer questions
-
-  ]).then(userChoice => {
-    // STUDENT: Make sure the id supplied is unique, then take the data supplied and 
-    // instantiate the Engineer constructor.
-    
-    
-    // STUDENT: When finished:
-       // Add the new object to the team member array
-       // Pass control back to the createTeam() function
-
-  });
-}
-
-// STUDENT: Now create a function for creating an Intern using the code above as an example
-
-
-// STUDENT: This function will call the render function required near the top (line 12), 
-// and pass INTO it the teamMembers area; from there, write the HTML returned back to a file 
-// in a directory called output.
-function renderHtmlPage(){
-  const html = render(teamMembers)
-  fs.writeFile("output/index.html", html, err => {
-
-  })
-}
-
-// This is our starter function.
-// Note that we use separate functions for different questions in inquirer to 
-// help keep code organized.
-function startMenu() {
-
-  // Here we start things off by calling the createManager function
-  createManager()
-
-}
-
-
-// HINT: make sure to build out your classes first! Remember that your Manager, Engineer,
-// and Intern classes should all extend from a class named Employee; see the directions
-// for further information. Be sure to test out each class and verify it generates an
-// object with the correct structure and methods. This structure will be crucial in order
-// for the provided `render` function to work! ```
-
-
-startMenu();
